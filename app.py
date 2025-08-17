f23106dae5e7f6f1a965b8a29cafaa69f615c63b
BYulia911 <bakharevayulia34@gmail.com>
2025-08-17 14:22:25 +0300

    Добавление email в регистрации

--- app.py
+++ app.py
@@ -3,20 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 import bcrypt
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Необходимо для использования flash-сообщений
+app.secret_key = 'your_secret_key'
 
-# Настройка подключения к базе данных PostgreSQL
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost:5432/run_sphere'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Модель пользователя
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.LargeBinary, nullable=False)
 
-# Создание базы данных
 with app.app_context():
     db.create_all()
 
@@ -28,15 +26,16 @@ def home():
 def register():
     if request.method == 'POST':
         username = request.form.get('username')
+        email = request.form.get('email')
         password = request.form.get('password')
 
-        if User.query.filter_by(username=username).first():
+        if (User.query.filter_by(username=username).first() or
+            User.query.filter_by(email=email).first()):
             flash("Пользователь уже существует")
-            return redirect(url_for('home'))
+            return render_template('register.html', email=email, username=username)
 
-        # Шифрование пароля
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-        new_user = User(username=username, password=hashed_password)
+        new_user = User(username=username, email=email, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
 
@@ -56,7 +55,6 @@ def login():
             flash("Пользователь не найден")
             return redirect(url_for('home'))
 
-        # Проверка пароля
         if bcrypt.checkpw(password.encode('utf-8'), user.password):
             flash("Успешный вход")
             return redirect(url_for('home'))
